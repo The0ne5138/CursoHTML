cdf872433d7c1a10e5f1ed33e6c4ab1b3a82208b
Eppure si muove <daguirre5138@gmail.com>
2023-03-18 11:52:15 -0300

    Ctualizacoin del sabado 18/3/23. A ponerse las pilas

---
+++ Clase 5/ejercicio1.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingrese nombre");
+let apellido = prompt("Ingres apellido.");
+let cantidadDeEntradas = parseInt(prompt("Ingrese la cantidad de entradas a comprar" )); 
+const precio = 500;
+let salida = nombre + " " + apellido + " Gracias por su compra! El precio tatal es: $ " + cantidadDeEntradas*500 ;
+alert(salida);
---
+++ Clase 5/ejercicio2.js
@@ -0,0 +1,15 @@
+let base = parseInt(prompt("Ingrese la base de el rectangulo "));
+let altura = parseInt(prompt("Ingrese la altura de el rectangulo "));
+perimetro = 2*(base + altura);
+superficie = base*altura;
+salida = "El perímetro del rectángulo es: " + perimetro + " y su superficie es: " + superficie ;
+alert(salida);
+// comentario de una linea: es con dos barras
+
+/* comentario de varias lineas es: abre /* y cierra con similar al (liena 1)
+ final de estos comentarios la pongo xD, por que si la pongo ahora (linea 2)
+se me termina lo comentado y empieza el codigo nuevamente          (linea 3)
+.
+.
+.
+(linea N-esima) los simbolos de finalizacion son:  */ 
